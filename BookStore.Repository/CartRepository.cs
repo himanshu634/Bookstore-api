e65bd967baf606041c40c97fa2242ceb3541a623
Himashu Mendapara <himanshumendapra@gmail.com>
2022-07-01 13:15:20 +0530

    cart model added

---
+++ BookStore.Repository/CartRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.models.Models;
+using BookStore.models.ViewModels;
+
+namespace BookStore.Repository
+{
+    public class CartRepository : BaseRepository
+    {
+        public List<Cart> GetCartItems(string keyword)
+        {
+            keyword = keyword?.ToLower()?.Trim();
+            var query = _context.Carts.Include(c => c.Book).Where(c => keyword == null || c.Book.Name.ToLower().Contains(keyword)).AsQueryable();//Where(c => keyword == null || c.Id )
+            return query.ToList();
+        }
+        public Cart GetCart(int Id)
+        {
+            return _context.Carts.FirstOrDefault(c => c.Id == Id);
+         }
+
+        public Cart AddCart(Cart cart)
+        {
+            var entry = _context.Carts.Add(cart);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public Cart UpdateCart(Cart cart)
+        {
+            var entry = _context.Carts.Update(cart);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public bool DeleteCart(int Id)
+        {
+            var cart = _context.Carts.FirstOrDefault(c => c.Id == Id);
+            if(cart == null)
+            {
+                return false;
+            }
+
+            _context.Carts.Remove(cart);
+            _context.SaveChanges();
+
+            return true; 
+        }
+
+    }
+
+    
+}
+
---
+++ bookstore/Controllers/CartController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookStore.models.Models;
+using BookStore.Repository;
+using BookStore.models.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace BookStore.Api.Controllers
+{
+
+    [ApiController]
+    [Route("cart")]
+    public class CartController : Controller
+    {
+        CartRepository _cartRepository = new CartRepository();
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetCartItems(string keyword)
+        {
+            List<Cart> carts = _cartRepository.GetCartItems(keyword);
+            IEnumerable<CartModel> cartModels = carts.Select(c => new CartModel(c)); 
+            return Ok(cartModels);
+        }
+
+        [HttpPost]
+        [Route("add")]    
+       public IActionResult AddCart(CartModel cartModel)
+        {
+            if(cartModel == null)
+            {
+                return BadRequest();
+            }
+            Cart cart = new Cart()
+            {
+                Id = cartModel.Id,
+                Bookid = cartModel.BookId,
+                Quantity = 1,
+                Userid = cartModel.UserId,
+            };
+
+            return Ok(_cartRepository.AddCart(cart));
+
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult UpdateCart(CartModel cartModel)
+        {
+            if(cartModel == null)
+            {
+                return BadRequest();
+            }
+
+            Cart cart = new Cart()
+            {
+                Id = cartModel.Id,
+                Userid = cartModel.UserId,
+                Bookid = cartModel.BookId,
+                Quantity = cartModel.Quantity,
+            };
+
+            return Ok(_cartRepository.UpdateCart(cart));
+        }
+
+
+        [HttpDelete]
+        [Route("delete")]
+        public IActionResult DeleteCart(int Id)
+        {
+            if(Id == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_cartRepository.DeleteCart(Id));
+        }
+    }
+
+}
